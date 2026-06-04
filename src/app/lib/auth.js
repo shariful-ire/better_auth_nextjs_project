a4130b585bb2657ec9f0a97865a01d28547cad02
shariful-ire <shariful.ire@gmail.com>
2026-06-04 11:12:13 +0600

    import mongodb

--- src/app/lib/auth.js
+++ src/app/lib/auth.js
@@ -1,4 +1,6 @@
 import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
 export const auth = betterAuth({
   //...
